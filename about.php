8c0bd8157607747a609d1a904e688578391ab184
unknown <nikepick@gmail.com>
2019-10-30 02:33:43 +0530

    updated about

--- about.php
+++ about.php
@@ -1,3 +1,3 @@
-<?php
-    echo 'working <3';
-?>
+<a href="/aboutdam.html">About Dan</a>
+<a href="https://nikepick.github.io/aboutme/">About Nikhil</a>
+<a href="https://asmitaupreti.github.io/AboutMe/AboutMe.html">About Asmita</a>
