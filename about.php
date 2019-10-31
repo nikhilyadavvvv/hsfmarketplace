d190d24b9fc030d880f722662dc289f4ed5e2da9
Asmita Upreti <asmitaupreti52@gmail.com>
2019-10-31 11:10:43 +0100

    linked individual pages to main page

--- about.php
+++ about.php
@@ -1,4 +1,6 @@
 <a href="/aboutdam.html">About Dan</a>
 <a href="https://nikepick.github.io/aboutme/">About Nikhil</a>
 <a href="/Asmita.html">About Asmita</a>
-<a href="/kumkum.html">About Asmita</a>
+<a href="/kumkum.html">About KumKum</a>
+<a href="/usmanaslam.html">About Usman</a>
+
