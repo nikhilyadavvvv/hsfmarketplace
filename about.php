0f84c6eb99cf925963bf4317bd9b8c51ec0bba15
unknown <nikepick@gmail.com>
2019-10-31 20:09:08 +0530

    db connection established

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <a href="/aboutdam.html">About Dan</a>
-<a href="https://nikepick.github.io/aboutme/">About Nikhil</a>
+<a href="/about_nik.html">About Nikhil</a>
 <a href="/Asmita.html">About Asmita</a>
 <a href="/kumkum.html">About KumKum</a>
 <a href="/usmanaslam.html">About Usman</a>
--- index.php
+++ index.php
@@ -4,5 +4,54 @@
 </head>
 <body>
     <a href="/about.php">About</a>
+
+<table border="1">
+<thead>
+    <tr>
+        <td>
+            S.N.
+        </td>
+        <TD>
+            Name
+        </TD>
+        <td>
+            User Type
+        </td>
+    </tr>
+</thead>
+
+    <?php
+        include 'db.php';
+        $sql = "SELECT * FROM `user`";
+$result = mysqli_query($mysqli,$sql);
+$sn = 0;
+
+
+if($result){
+while($row = $result -> fetch_assoc()){
+
+    echo "<tr>";
+
+   echo "<td>";
+   echo $sn++;
+   echo "</td>";
+
+   echo "<td>";
+   echo $row["username"];
+   echo "</td>";
+
+    echo "<td>";
+   echo $row["user_type"];
+   echo "</td>";
+
+
+   echo "</tr>";
+   }
+   echo "<script>alert('if the table is visible... Database is hosted properly');</script>";
+}
+
+
+?>
+</table>
 </body>
 </html>
