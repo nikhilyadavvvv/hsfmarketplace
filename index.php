ef4cf12590fd15fc911bd8197b917f5039617321
jobayermojumder <jobayer.pro@gmail.com>
2019-11-07 11:01:55 +0100

    project about change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include('include/header.php');
     <!-- Second Container -->
     <div class="container-fluid bg-2 text-center">
         <h3 class="margin">Who we are?</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <p>"HSF-Marketplace" is an e-commerce for the students of Hochschule Fulda</p>
     </div>
 <?php
 include('include/footer.php');
