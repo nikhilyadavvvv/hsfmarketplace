90686203b9a564ae610a66157de4d904feaab6b1
jobayermojumder <jobayer.pro@gmail.com>
2019-11-21 13:05:20 +0100

    Pipleline test for AWS server

--- include/header.php
+++ include/header.php
@@ -62,7 +62,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>                        
                 </button>
-                <a class="navbar-brand" href="index.php">HSMARKETPLACE</a>
+                <a class="navbar-brand" href="index.php">HSMARKETPLACE 3</a>
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav navbar-right">
