3eb7611e10b0f70520c208a3363d7730f1f1ac12
jobayermojumder <jobayer.pro@gmail.com>
2019-12-12 13:08:07 +0100

    header template changed

--- demo/model/auth.php
+++ demo/model/auth.php
@@ -1,21 +1,24 @@
 <?php
-    require 'db.php';
-    $email = mysqli_real_escape_string($mysqli,$_POST['email']);
-    $password = mysqli_real_escape_string($mysqli,$_POST['password']);
-    $password = md5($password);
-    $flag = 0;
-    $sql = "SELECT *  FROM `user` WHERE `email` = '".$email."' AND `password` = '".$password."'";
-    $result = mysqli_query($mysqli, $sql);
-    $count = mysqli_num_rows($result);
-    if ($count==1) {
-            while ($row = $result->fetch_assoc()) {
-                echo 'valid credentials';
-                $_SESSION['user_id'] = $row["user_id"];
-        }
-    } else {
-        $_SESSION['error_message'] = 'Invalid credentials.';
-        header("Location: ../login.php"); 
+require 'db.php';
+$email = mysqli_real_escape_string($mysqli,$_POST['email']);
+$password = mysqli_real_escape_string($mysqli,$_POST['password']);
+$password = md5($password);
+$flag = 0;
+$sql = "SELECT *  FROM `user` WHERE `email` = '".$email."' AND `password` = '".$password."'";
+$result = mysqli_query($mysqli, $sql);
+$count = mysqli_num_rows($result);
+if ($count==1) {
+    while ($row = $result->fetch_assoc()) {
+        echo 'valid credentials';
+        $_SESSION['user_id'] = $row["user_id"];
+        $_SESSION['user_name'] = $row["firstname"];
+        header("Location: ../index.php"); 
         exit();
     }
-    $mysqli->close();
+} else {
+    $_SESSION['error_message'] = 'Invalid credentials.';
+    header("Location: ../login.php"); 
+    exit();
+}
+$mysqli->close();
 ?>
