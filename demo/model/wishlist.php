1daa5b32576bb0273732f8b5529e606df7c12458
jobayermojumder <jobayer.pro@gmail.com>
2019-12-14 23:56:22 +0100

    wishlist remove for unregister user

--- demo/model/wishlist.php
+++ demo/model/wishlist.php
@@ -1,28 +1,56 @@
 <?php
 require 'db.php';
-if(empty($_SESSION['user_id'])){
-    echo "You are not logged in ";
-    exit();
-}
-$user_id = $_SESSION['user_id'];
+
 $product_id = $email = mysqli_real_escape_string($mysqli,$_GET['product_id']);
-$sql = "SELECT *  FROM `wishlist` WHERE `user_id` = '".$user_id."' AND `product_id` = '".$product_id."'";
-$result = mysqli_query($mysqli, $sql);
-$count = mysqli_num_rows($result);
-if ($count==0) {
-    $insertSql = "INSERT INTO `wishlist` (`user_id`, `product_id`) VALUES ('$user_id', '$product_id')";
+if(!isset($_SESSION['user_id']) || !empty($_SESSION['user_id'])){
+
+    $data =  array();
+    if (!isset($_SESSION['wishlist'])) {
+        $_SESSION['wishlist'] = $data;
+    }
+    $sql = "SELECT * FROM table_product where id = ".$product_id;
+    $result = mysqli_query($mysqli,$sql);
+    $json_array = array();
+    if($result){
+        while($row = $result -> fetch_assoc()){
+            $data['id'] = $row['id'];
+            $data['name'] = $row['name'];
+            $data['image'] = $row['image'];
+            $data['cost'] = $row['cost'];
+            $data['category'] = $row['category'] ;
+            $data['description'] = $row['description'];
+            $data['stock'] = $row['stock'];
+        }
+    }
+    array_push($_SESSION['wishlist'], $data);
+
+    //echo "<pre>";
+
+    echo "Product added to wishlist";
+
+
+}else{
+
+
+    $user_id = $_SESSION['user_id'];
+    $sql = "SELECT *  FROM `wishlist` WHERE `user_id` = '".$user_id."' AND `product_id` = '".$product_id."'";
+    $result = mysqli_query($mysqli, $sql);
+    $count = mysqli_num_rows($result);
+    if ($count==0) {
+        $insertSql = "INSERT INTO `wishlist` (`user_id`, `product_id`) VALUES ('$user_id', '$product_id')";
     //$insertResult = mysqli_query($mysqli, $insertSql);
 
-    if (mysqli_query($mysqli,$insertSql)) {
-        echo "Product added to wishlist";
-    } else {
-        echo mysqli_error($mysqli);
+        if (mysqli_query($mysqli,$insertSql)) {
+            echo "Product added to wishlist";
+        } else {
+            echo mysqli_error($mysqli);
+        }
+
+
     }
-
-
+    else{
+        echo "Product already in wishlist";
+    }
+    $mysqli->close();
 }
-else{
-    echo "Product already in wishlist";
-}
-$mysqli->close();
 ?>
