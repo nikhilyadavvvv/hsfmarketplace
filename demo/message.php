235af7d6b33a5ab11ef06a314d93edbc3edb1112
unknown <nikepick@gmail.com>
2019-12-15 14:11:13 +0530

    messages done

--- demo/message.php
+++ demo/message.php
@@ -3,8 +3,9 @@
 
 <head>
     <?php include('include/header_style.php');
-     error_reporting(0);
-     ?>
+    error_reporting(0);
+    ?>
+     <script src="js/site/message.js"></script>
 </head>
 
 <body onload="loadCart()">
@@ -46,107 +47,104 @@
                 <div class="row">
                     <!-- message table -->
                     <div class="table-content table-responsive">
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th class="product-thumbnail">S.N.</th>
-                                                    <th class="product-name">SENDER</th>
-                                                    <th class="product-quantity">PRODUCT</th>
-                                                    <th class="product-price">MESSAGE</th>
-                                                    <th class="product-subtotal">TIME</th>
-                                                    <th class="product-remove">Actions</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-<!-- MESSAGE -->
-                                            <?php
-                                                   require 'model/db.php';
-                                                   $i = 0;
-                                                   $user_id = $_SESSION['user_id'];
-                                                   $sql = "SELECT *  FROM `message` WHERE `receiver` = $user_id ORDER BY `message`.`message_id` DESC";
-                                                    $result = mysqli_query($mysqli,$sql);
-                                                    if($result){
-                                                        while($row = $result -> fetch_assoc()){
-                                                            $i++;
-                                                            $sender = $row['sender'];
-                                                            $content = $row['content'];
-                                                            $timestamp = $row['timestamp'];
-                                                            $product_id = $row['product_id'];
-                                                            $product_name ="";
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th class="product-thumbnail">S.N.</th>
+                                    <th class="product-name">SENDER</th>
+                                    <th class="product-quantity">PRODUCT</th>
+                                    <th class="product-price">MESSAGE</th>
+                                    <th class="product-subtotal">TIME</th>
+                                    <th class="product-remove">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- MESSAGE -->
+                                <?php
+                                require 'model/db.php';
+                                $i = 0;
+                                $user_id = $_SESSION['user_id'];
+                                $sql = "SELECT *  FROM `message` WHERE `receiver` = $user_id ORDER BY `message`.`message_id` DESC";
+                                $result = mysqli_query($mysqli, $sql);
+                                if ($result) {
+                                    while ($row = $result->fetch_assoc()) {
+                                        $i++;
+                                        $sender = $row['sender'];
+                                        $content = $row['content'];
+                                        $timestamp = $row['timestamp'];
+                                        $product_id = $row['product_id'];
+                                        $product_name = "";
 
-                                                           $sql2 = "SELECT *  FROM `user` WHERE `user_id` = $sender";
-                                                            $result2 = mysqli_query($mysqli,$sql2);
-                                                            while($row2 = $result2 -> fetch_assoc()){
-                                                                $sender = $row2["firstname"];
-                                                            }
+                                        $sql2 = "SELECT *  FROM `user` WHERE `user_id` = $sender";
+                                        $result2 = mysqli_query($mysqli, $sql2);
+                                        while ($row2 = $result2->fetch_assoc()) {
+                                            $sender_name = $row2["firstname"];
+                                        }
 
-                                                            $sql3 = "SELECT *  FROM `table_product` WHERE `id` = $product_id";
-                                                            $result3 = mysqli_query($mysqli,$sql3);
-                                                            while($row3 = $result3 -> fetch_assoc()){
-                                                                $product_name = $row3["name"];
-                                                            }
-                                                            ?>
-                                                            <tr>
-                                                            <td class="product-name"><?php echo $i; ?></td>
-                                                            <td class="product-name"><?php echo $sender; ?></td>
-                                                            <td class="product-name"><a href="product_detail.php?id=<?php echo $product_id; ?>"><?php echo $product_name; ?></a></td>
-                                                            <td class="product-name"><?php echo $content; ?></td>
-                                                            <td class="product-name"><?php echo $timestamp; ?></td>
+                                        $sql3 = "SELECT *  FROM `table_product` WHERE `id` = $product_id";
+                                        $result3 = mysqli_query($mysqli, $sql3);
+                                        while ($row3 = $result3->fetch_assoc()) {
+                                            $product_name = $row3["name"];
+                                        }
+                                        ?>
+                                        <tr>
+                                            <td class="product-name"><?php echo $i; ?></td>
+                                            <td class="product-name"><?php echo $sender_name; ?></td>
+                                            <td class="product-name"><a href="product_detail.php?id=<?php echo $product_id; ?>"><?php echo $product_name; ?></a></td>
+                                            <td class="product-name"><?php echo $content; ?></td>
+                                            <td class="product-name"><?php echo $timestamp; ?></td>
 
-                                                            <td class="product-remove"><i class="icon-pencil icons"></i>
-                                                                <i class="icon-trash icons"></i>
-                                                            </td>
-                                                        </tr>
+                                            <td class="product-remove">
+                                                <a href="#" data-toggle="modal" data-target="#messageModal" onclick="setSender(<?php echo $sender;?>); setProduct(<?php echo $product_id;?>);">
+                                                    <i class="icon-pencil icons"></i>
+                                                </a>
+                                                <i class="icon-trash icons"></i>
+                                            </td>
+                                        </tr>
 
-                                                            <?php
+                                <?php
 
-                                                        }
-                                                    }
-                                                ?>
-<!-- END MESSAGE -->
-
-
-
-
-
-                                                <?php if($result){
-                                                    while($row = $result -> fetch_assoc()){ ?>
-                                                        <tr>
-                                                            <td class="product-thumbnail"><a href="product_detail.php?id=<?php echo $row['id'] ?>"><img src="<?php echo $row['thumbnail']; ?>" alt="product img" /></a></td>
-                                                            <td class="product-name"><a href="product_detail.php?id=<?php echo $row['id'] ?>"><?php echo $row['name']; ?></a>
-                                                            </td>
-                                                            
-                                                            <td class="product-name"><a href="#"><?php echo $row['category']; ?></a></td>
-                                                            
-                                                            <td class="product-price"><span class="amount">£<?php echo $row['cost'] ?></span></td>
-
-                                                            <td class="product-subtotal">
-                                                                <?php if ( $row['status'] == 'approved') { ?>
-                                                                    <span class="text-success">Approved</span>
-                                                                <?php } else { ?>
-                                                                    <span class="text-danger">Unapproved</span>
-                                                                <?php } ?>
-
-                                                            </td>
-                                                            <td class="product-remove"><a href="edit_products.php?id=<?php echo $row['id']; ?>"><i class="icon-pencil icons"></i></a>
-                                                                <a href="#" onclick="delete_product('<?php echo $row['id'] ?>')"><i class="icon-trash icons"></i></a>
-                                                            </td>
-                                                        </tr>
-                                                    <?php } } ?>
-                                                </tbody>
-                                            </table>
-                                        </div>
+                                    }
+                                }
+                                ?>
+                                <!-- END MESSAGE -->
+                            </tbody>
+                        </table>
+                    </div>
                     <!-- end message table -->
                 </div>
             </div>
         </section>
-                    <!-- End Product Description -->
-                    <?php include('include/footer.php') ?>
-                    <!-- End Footer Style -->
+        <!-- End Product Description -->
+        <?php include('include/footer.php') ?>
+
+        <div id="messageModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Message to Seller</h4>
+                    </div>
+                    <form action="model/message.php" method="POST">
+
+                        <div class="modal-body" style="padding: 5px;">
+                            <input name="to" type="hidden" name="to" value="" id="toSender" ** />**
+                            <input name="product_id" type="hidden" name="to" value="" id="sendProductId" ** />**
+                            <textarea id="content" class="form-group margin-0" placeholder="Type your message here..."></textarea>
+                        </div>
+                        <div class="modal-footer" style="padding: 5px;">
+                            <button type="submit" onclick="sendMessage()" class="btn btn-primary" data-dismiss="modal">Send Message</button>
+                        </div>
+                    </form>
                 </div>
 
-                <?php include('include/footer_js.php') ?>
+                <!-- End Footer Style -->
+            </div>
 
-            </body>
+            <?php include('include/footer_js.php') ?>
 
-            </html>
+</body>
+
+</html>
