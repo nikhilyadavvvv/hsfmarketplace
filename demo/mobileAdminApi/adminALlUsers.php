2bfdadf607a96d3db10c78a40e083d07851d5a3c
Asmita Upreti <asmitaupreti52@gmail.com>
2020-02-02 15:42:17 +0100

    mobile admin api

---
+++ demo/mobileAdminApi/adminALlUsers.php
@@ -0,0 +1,20 @@
+<?php
+include 'db.php';
+$sql = "SELECT * FROM `user`";
+
+$result = mysqli_query($mysqli,$sql);
+$json_array = array();
+if($result){
+    while($row = $result -> fetch_assoc()){
+        $data =  array();
+        $data['id'] = $row['user_id'];
+        $data['name'] = $row['firstname'] . $row['lastname'] ;
+        $data['status'] = $row['status'];
+        $data['isSeller'] = $row['is_seller'];
+       $json_array[] = $data;
+    }
+    $json_array = json_encode($json_array);
+    header('Content-Type: application/json');
+    print_r($json_array);
+}
+?>
