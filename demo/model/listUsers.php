e1a6796f8c1b9250cff7edb78a57372a8dcd1428
unknown <nikepick@gmail.com>
2020-02-06 21:37:27 +0530

    listUsers

--- demo/model/listUsers.php
+++ demo/model/listUsers.php
@@ -10,7 +10,7 @@ if ($verifier === "x1x2x3x4x5x6x7x8x9") {
     while ($row = $result->fetch_assoc()) {
         $data =  array();
         $data['id'] = $row["user_id"];
-        $sql2="SELECT * FROM `basket` WHERE `basket`.user_id = "+$row["user_id"];
+       echo $sql2="SELECT * FROM `basket` WHERE `basket`.user_id = "+$row["user_id"];
         $resultx = mysqli_query($mysqli, $sql2);
         $count = mysqli_num_rows($resultx);
         $data['count'] = $count;
@@ -18,7 +18,7 @@ if ($verifier === "x1x2x3x4x5x6x7x8x9") {
         $json_array[] = $data;
     }
     $json_array = json_encode($json_array);
-    header('Content-Type: application/json');
+    //header('Content-Type: application/json');
     print_r($json_array);
 }
 
