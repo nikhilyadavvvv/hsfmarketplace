5fd9edcd4ff6ab143da548eccb6fb09bafeb1c4d
Asmita Upreti <asmitaupreti52@gmail.com>
2020-02-07 16:11:27 +0100

    login api updated

--- demo/mobileAdminApi/adminLogin.php
+++ demo/mobileAdminApi/adminLogin.php
@@ -12,23 +12,23 @@ $result = mysqli_query($mysqli, $sql);
 $count = mysqli_num_rows($result);
 
 $json_array = array();
-
+$data =  array();
 
 if ($count == 1) {
    
 
     while ($row = $result->fetch_assoc()) {
-        $data =  array();
+        
         $data['id'] = $row['id'];
         $data['name'] = $row['user_name'];
         $data['password'] = $row['password'];
-        $json_array[] = $data;
+        
            
         } 
     }
 
 
-    $json_array = json_encode($json_array);
+    $json_array = json_encode($data);
     header('Content-Type: application/json');
     print_r($json_array);
 
