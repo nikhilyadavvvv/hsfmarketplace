22cf7072f877afbed999416cdbee3a900f7abd63
Asmita Upreti <asmitaupreti52@gmail.com>
2020-02-07 19:38:21 +0100

    new apis for admin

---
+++ demo/mobileAdminApi/adminCountCategory.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+$sql = "SELECT category_name,COUNT(p.category_id ) AS 'total_product' FROM product_category c LEFT OUTER  JOIN table_product p ON c.category_id = p.category_id GROUP BY category_name";
+
+$result = mysqli_query($mysqli,$sql);
+
+$json_array = array();
+$id = 1;
+if($result){
+    while($row = $result -> fetch_assoc()){
+        $data =  array();
+        $data['id'] = $id;
+        $data['categoryName'] = $row['category_name'];
+        $data['totalProduct'] = $row['total_product'];
+        $id++;
+       $json_array[] = $data;
+    }
+    $json_array = json_encode($json_array);
+    header('Content-Type: application/json');
+    print_r($json_array);
+}
+?>
