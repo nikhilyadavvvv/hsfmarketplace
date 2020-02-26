86502f26ae4a6d2ffd6655269b77f5aefd17b222
Asmita Upreti <asmitaupreti52@gmail.com>
2020-02-27 00:43:28 +0100

    .

--- demo/mobileAdminApi/adminProfileData.php
+++ demo/mobileAdminApi/adminProfileData.php
@@ -5,7 +5,7 @@ $json = file_get_contents('php://input');
 $data = json_decode($json);
 
 $userId = mysqli_real_escape_string($mysqli, $data->id);
-$email = mysqli_real_escape_string($mysqli, $data->user);
+$email = mysqli_real_escape_string($mysqli, $data->email);
 $password = mysqli_real_escape_string($mysqli, $data->password);
 
 $sql = "UPDATE `admin_accounts` SET `user_name` = '" . $email . "' AND `password` = '" . $password . "' WHERE `user_id` = '" . $userId."'";;
