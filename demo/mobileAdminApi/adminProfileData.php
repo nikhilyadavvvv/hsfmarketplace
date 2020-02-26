bc6068d9cc7c94910b8e7068b27a54689d06ea74
Asmita Upreti <asmitaupreti52@gmail.com>
2020-02-27 00:37:21 +0100

    .

--- demo/mobileAdminApi/adminProfileData.php
+++ demo/mobileAdminApi/adminProfileData.php
@@ -10,11 +10,12 @@ $password = mysqli_real_escape_string($mysqli, $data->password);
 
 $sql = "UPDATE `admin_accounts` SET `user_name` = '" . $email . "' AND `password` = '" . $password . "' WHERE `user_id` = '" . $userId."'";;
 $result = mysqli_query($mysqli, $sql);
-
+var_dump(result)
 $json_array = array();
 if(result){
     $data = array();
     $data['message'] = "Success";
+    $json_array[] = $data;
 }
 $json_array = json_encode($json_array);
     header('Content-Type: application/json');
